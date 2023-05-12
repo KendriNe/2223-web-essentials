b0af98dc53089e9fd786d6949501522b1e218d9a
Kenji Neijens <12100409@student.pxl.be>
2023-05-12 11:35:12 +0200

    vue reddevils

--- Semester 2/Week 9/reddevils/src/components/icons/playercard.vue
+++ Semester 2/Week 9/reddevils/src/components/icons/playercard.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-
+    <p><img v-bind:src="'src/assets/' + this.selectedplayer.afbeelding"></p>
+    <p>{{this.selectedplayer.speler}}</p>
+    <p>{{this.selectedplayer.geboortedatum}}</p>
+    <p>{{this.selectedplayer.club}}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "playercard"
+  name: "playercard",
+  props : {
+    selectedplayer : {type : Object}
+  },
 }
 </script>
 
 <style scoped>
-
+  img {
+    width: 150px;
+    height: 150px;
+  }
+  div {
+    float: left ;
+    width: 33%;
+    margin-bottom: 100px;
+  }
 </style>
--- Semester 2/Week 9/reddevils/src/views/playerscards.vue
+++ Semester 2/Week 9/reddevils/src/views/playerscards.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-
+    <playercard
+        v-for="speler in this.players"
+        v-bind:selectedplayer = speler
+    >
+    </playercard>
   </div>
 </template>
 
 <script>
+import Playercard from "@/components/icons/playercard.vue";
 export default {
-  name: "playerscards"
+  name: "playerscards",
+  components: {Playercard},
+
+  data() {
+    return {
+      players : [
+        {id:0 , speler : "Thibaut Courtois", geboortedatum: "11/05/1992", club:"Real Madrid", afbeelding: "courtois.png"},
+        {id:1 , speler : "Toby Alderweireld", geboortedatum: "02/03/1989", club:"Al-Duhail SC", afbeelding: "alderweireld.png"},
+        {id:2 , speler : "Christian Benteke", geboortedatum: "03/09/1990", club:"Crystal Palace", afbeelding: "benteke.png"},
+        {id:3 , speler : "Yannick Carrasco", geboortedatum: "04/09/1993", club:"Atlético Madrid", afbeelding: "carrasco.png"},
+        {id:4 , speler : "Kevin Debruyne", geboortedatum: "28/06/1991", club:"Manchester City", afbeelding: "debruyne.png"},
+        {id:5 , speler : "Jason Denayer", geboortedatum: "28/06/1995", club:"Olympic Lyon", afbeelding: "denayer.png"},
+        {id:6 , speler : "Eden Hazard", geboortedatum: "07/01/1991", club:"Real Madrid", afbeelding: "hazard.png"},
+        {id:7 , speler : "Romelu Lukaku", geboortedatum: "13/05/1993", club:"Chelsea FC", afbeelding: "lukaku.png"},
+        {id:8 , speler : "Dries Mertens", geboortedatum: "06/05/1987", club:"SSC Napoli", afbeelding: "mertens.png"},
+        {id:9 , speler : "Thomas Meunier", geboortedatum: "12/09/1991", club:"Borussia Dortmund", afbeelding: "meunier.png"},
+        {id:10 , speler : "Youri Tielemans", geboortedatum: "07/05/1997", club:"Leicester City", afbeelding: "tielemans.png"},
+        {id:11 , speler : "Leandro Trossard", geboortedatum: "04/12/1994", club:"Brighton & Hove Albionver", afbeelding: "trossard.png"},
+        {id:12 , speler : "Jan Verthongen", geboortedatum: "24/04/1987", club:"SL Benfica", afbeelding: "verthongen.png"},
+        {id:13 , speler : "Axel Witsel", geboortedatum: "12/01/1989", club:"Borussia Dortmund", afbeelding: "witsel.png"}
+      ]
+    }
+  }
 }
 </script>
 
